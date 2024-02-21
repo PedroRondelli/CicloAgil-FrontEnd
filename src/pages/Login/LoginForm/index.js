d8fabf0b7f542c67777bce6eda7046038271321c
PedroRondelli <pedrorondelli@hotmail.com>
2024-02-21 15:05:29 -0300

    create global-styles folder

---
+++ src/pages/Login/LoginForm/index.js
@@ -0,0 +1,10 @@
+import React, { useRef } from "react";
+import { View } from "react-native";
+
+import { Container } from "./styles";
+
+const LoginForm = () => {
+  return <Container/>
+};
+
+export default LoginForm;
